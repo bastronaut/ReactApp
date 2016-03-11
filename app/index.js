88db74a6fbcd0716e069f077b3fece665c58e9f7
bastronaut <btsijtsma@gmail.com>
2016-03-11 18:43:32 +0100

    test nested components

--- app/index.js
+++ app/index.js
@@ -1,13 +1,54 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Hello = React.createClass({
-  render: function () {
-    return (<div>Test transpiler</div>);
+var user = {
+  'name' : 'bas',
+  'githubusername' : 'bastronaut',
+  'homepage' : 'bastronaut.com'
+}
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.name}</div>
+    )
   }
 })
 
+var Github = React.createClass({
+  render: function() {
+    return(
+      <div>
+        <a href={"http://www.github.com/"+ this.props.githubusername}>Github</a>
+      </div>
+    )
+  }
+})
+
+var Website = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={"http://www."+ this.props.homepage}>{this.props.homepage}</a>
+      </div>
+    )
+  }
+})
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfileName name={this.props.user.name} />
+        <Github githubusername={this.props.user.githubusername} />
+        <Website homepage={this.props.user.homepage} />
+      </div>
+    )
+  }
+})
+
+
 ReactDOM.render(
-  <Hello/>,
+  <Avatar user={user}/>,
   document.getElementById('app')
 );
