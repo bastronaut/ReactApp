083fc4d7a5643df4296468b92d19f11699d5050e
bastronaut <btsijtsma@gmail.com>
2016-03-12 21:23:35 +0100

    added reactrouter and components

---
+++ app/components/Main.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Hi from Main
+      </div>
+    )
+  }
+});
+
+module.exports = Home;
